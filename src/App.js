a358efd84aa71a3ca5792d1d88b5c5eb4379af61
Jorge Barcos <jorgebarcos88@gmail.com>
2019-10-05 09:07:49 -0300

    Creando la App

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="container">
+			<header>
+				<h1 className="text-center">Administrador de Pacientes de Veterinaria</h1>
+			</header>
+
+			<div className="row mt-3">
+				<div className="col-md-6">Formulario aqui</div>
+				<div className="col-md-6">Listado Aqui</div>
+			</div>
+		</div>
+	);
 }
 
 export default App;
