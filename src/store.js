7f100c94be5b1aa900c49504895935879b07b378
Jorge Barcos <jorgebarcos88@gmail.com>
2019-10-05 11:17:24 -0300

    Almacenando Citas en el Storage

--- src/store.js
+++ src/store.js
@@ -1,14 +1,24 @@
 import { createStore } from 'redux';
 
 import reducer from './reducers';
+import { obtenerStateStorage, guardarStateStorage } from './localstorage';
 
 // Definir state inicial
 // const initialState = [];
 
+// Obtener citas de localstorage
+const storageState = obtenerStateStorage();
+
 const store = createStore(
 	reducer,
-	// initialState,
+	storageState,
 	window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
+
+store.subscribe(() => {
+	guardarStateStorage({
+		citas: store.getState().citas
+	});
+});
 
 export default store;
